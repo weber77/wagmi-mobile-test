d5f68d41ed408a3a0f4f56805f1763f419677606
weber77 <weberkanga77@gmail.com>
2024-06-26 06:46:41 +0100

    mobile check

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,11 +1,11 @@
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { connectorsForWallets, getDefaultConfig } from "@rainbow-me/rainbowkit";
 
 import { mainnet } from "wagmi/chains";
-// import {
-//     walletConnectWallet,
-//     metaMaskWallet,
-//     phantomWallet
-// } from "@rainbow-me/rainbowkit/wallets";
+import {
+    walletConnectWallet,
+    metaMaskWallet,
+
+} from "@rainbow-me/rainbowkit/wallets";
 
 import { createConfig, http } from "wagmi";
 import { metaMask, walletConnect } from "wagmi/connectors";
@@ -21,25 +21,30 @@ export const rainbowConfig = getDefaultConfig({
     ssr: true, // If your dApp uses server side rendering (SSR)
 });
 
-// const connectors = connectorsForWallets([
-//     {
-//         groupName: "Recommended",
-//         wallets: [metaMaskWallet, walletConnectWallet, phantomWallet],
-//     },
-// ], { projectId: PROJECT_ID, walletConnectParameters: {}, appName: "PaalX Sniper🎯", appDescription: "", appUrl: "", appIcon: "" });
+const connectors = connectorsForWallets([
+    {
+        groupName: "Recommended",
+        wallets: [metaMaskWallet, walletConnectWallet],
+    },
+], { projectId: PROJECT_ID, walletConnectParameters: {}, appName: "PaalX Sniper🎯", appDescription: "", appUrl: "", appIcon: "" });
 
 
 
+const wagmiWallets = [walletConnect({
+    projectId: PROJECT_ID, metadata: {
+        name: 'PaalAI',
+        description: "x",
+        url: "https://wagmi-paal.vercel.app/",
+        icons: [""]
+    }
+}), metaMask()]
+
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
 
 export const wagmiConfig = createConfig({
-    connectors: [walletConnect({
-        projectId: PROJECT_ID, metadata: {
-            name: 'PaalAI',
-            description: "x",
-            url: "https://wagmi-paal.vercel.app/",
-            icons: [""]
-        }
-    }), metaMask()],
+    connectors: isMobileDevice() ? wagmiWallets : connectors,
     chains: [mainnet],
     transports: {
         [mainnet.id]: http(''),
